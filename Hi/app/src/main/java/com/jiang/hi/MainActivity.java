c671397047adfa2f1a0918c3a9ee28906c448e1a
JTianyi <452436674@qq.com>
2016-07-11 11:31:21 +0800

    功能1 完成

--- Hi/app/src/main/java/com/jiang/hi/MainActivity.java
+++ Hi/app/src/main/java/com/jiang/hi/MainActivity.java
@@ -10,6 +10,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //功能1开始敲代码吗烦啊是否电脑刚才卡萨丁你烦啥地方Vlsdv
+        //功能1完成 
 
     }
 
